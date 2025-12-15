cd163f87d94c3b2b3fe3ed93ace2b222d0c076cc
MarcinOsko <119833974+MarcinOsko@users.noreply.github.com>
2025-12-15 06:06:59 +0100

    Merge remote-tracking branch 'origin/dev' into dev

--- DAY_01/Day_1_Secret_Entrance_part_2.py
+++ DAY_01/Day_1_Secret_Entrance_part_2.py
@@ -7,18 +7,18 @@ with (open("Day_1_Secret_Entrance_data.txt", "r") as f):
         steps = int(line[1:])
         if line[0] == "L":
             if current_pos == 0:
-                hits = steps // 100
+                hit = steps // 100
             else:
                 move = current_pos - steps
                 if move > 0:
-                    hits = 0
+                    hit = 0
                 else:
-                    hits = 1 + ((-move) // 100)
-            password += hits
+                    hit = 1 + ((-move) // 100)
+            password += hit
             current_pos = (current_pos - steps) % 100
         else:
-            hits = (current_pos + steps) // 100
-            password += hits
+            hit = (current_pos + steps) // 100
+            password += hit
             current_pos = (current_pos + steps) % 100
 
 print(f">>> PASWORD: {password}")
