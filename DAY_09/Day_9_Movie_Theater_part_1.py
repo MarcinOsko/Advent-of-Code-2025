76de68d45ba568864a2f354f9197073e3cab955f
MarcinOsko <119833974+MarcinOsko@users.noreply.github.com>
2025-12-29 10:43:12 +0100

    Advent of Code 2025

--- DAY_09/Day_9_Movie_Theater_part_1.py
+++ DAY_09/Day_9_Movie_Theater_part_1.py
@@ -1,12 +1,24 @@
-import pprint
-from pprint import pprint
+import itertools
 
+points = []
+pairs = []
+resoult = 0
 
-POINTS = []
-
-with open("test.txt", "r") as f:
+with open("Day_9_Movie_Theater_input.txt", "r") as f:
     for line in f:
-        POINTS.append(tuple(map(int, line.split(","))))
+        points.append(tuple(map(int, line.split(","))))
 
-pprint(POINTS)
+rectangle_field = 0
 
+def rect_field(p1, p2):
+    edge1 = abs(p2[0] - p1[0]) + 1
+    edge2 = abs(p2[1] - p1[1]) + 1
+    return  edge1 * edge2
+
+pairs  = list(itertools.combinations(points, 2))
+
+for rect in itertools.combinations(points, 2):
+    if rect_field(rect[0], rect[1]) > resoult:
+        resoult = rect_field(rect[0], rect[1])
+
+print(f'\n>>> The area of the largest possible rectangle is: {resoult}')
